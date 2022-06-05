27ad8b0bbb1d4bffed5a6d8b192c7f23d35a0481
[prasanna] <prasanna>
2022-06-05 16:14:14 +0530

    [prasanna]; unordered list of words in list

---
+++ UnOrderedList/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace UnOrderedList
+{
+    internal class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            LinkedListData list = new LinkedListData();
+            list.Add("prasanna");
+            list.Add("arjun");
+            list.Add("malli");
+            //list.DisPlay();
+            int a = list.Search("arjun");
+            Console.WriteLine("index position of element is: " + a);
+            list.InsertAtPerticularPosition(a + 1, "lakshmi");
+           // list.DisPlay();
+            list.RemoveFirstNode("prasanna");
+           // list.RemoveLastNode("malli");
+            list.DisPlay();
+        }
+    }
+}
+
